9242883913d2b4c8949ed20cc95dc707c3af69d3
Ariel Shurygin <arik.shurygin@gmail.com>
2024-10-31 23:20:06 +0000

    checkpoint, first draft of launcher, testing in separate experiment specific repo

--- src/scenarios_hpc_azure/__init__.py
+++ src/scenarios_hpc_azure/__init__.py
@@ -7,12 +7,23 @@ from .azure_utils import AzureExperimentLauncher
 
 # GLOBALS, DONT CHANGE UNLESS YOU KNOW WHAT YOU ARE DOING
 # tells the script where to look for certain files in an experiment
+# runner script filename
 RUNNER_SCRIPT_PATH = "run_task.py"
+# where experiment stores template configs
 TEMPLATE_CONFIGS_DIR_NAME = "template_configs"
+# where experiment stores state-specific config files
 STATE_CONFIGS_DIR_NAME = "states"
+# where experiment stores postprocessing scripts
 PP_SCRIPTS_DIR_NAME = "postprocessing_scripts"
+# optional directory where experiments are stored, if multiple experiments
+# are found in the same repo, they are found in this folder
 EXP_DIR_NAME_OPTIONAL = "exp"
+# path from user's CWD to where their azure configuration toml is stored
+SECRETS_DIR_NAME = "secrets"
+SECRETS_FILE_NAME = "configuration_cfaazurebatchprd.toml"
+# path to this package on users machine
 PACKAGE_PATH, _ = os.path.split(os.path.realpath(__file__))
+# path to the two data mapping files contained within this package
 FIPS_TO_NAME_PATH = os.path.join(PACKAGE_PATH, "data", "fips_to_name.csv")
 POPULATIONS_PATH = os.path.join(PACKAGE_PATH, "data", "CenPop2020_Mean_ST.csv")
 
--- src/scenarios_hpc_azure/launch_experiment.py
+++ src/scenarios_hpc_azure/launch_experiment.py
@@ -1,7 +1,9 @@
 import argparse
 import os
+from itertools import groupby
 
-EXPERIMENTS_DIRECTORY = os.getcwd()
+from . import EXP_DIR_NAME_OPTIONAL, SECRETS_DIR_NAME, SECRETS_FILE_NAME, utils
+from .azure_utils import AzureExperimentLauncher
 
 parser = argparse.ArgumentParser(description="Experiment Azure Launcher")
 parser.add_argument(
@@ -16,7 +18,7 @@ parser.add_argument(
     "--experiment_name",
     type=str,
     help="the experiment name, must match the experiment directory within %s"
-    % EXPERIMENTS_DIRECTORY,
+    % EXP_DIR_NAME_OPTIONAL,
     required=True,
 )
 parser.add_argument(
@@ -27,19 +29,69 @@ parser.add_argument(
     default=8,
     help="CPU count of machines running each task, supports 2, 4, or 8 cores",
 )
-args = parser.parse_args()
-experiment_name: str = args.experiment_name
-job_id: str = args.job_id
-cpu_count: int = args.cpu
+
+parser.add_argument(
+    "-t",
+    "--timeout",
+    type=int,
+    required=False,
+    default=60,
+    help="timeout time in minutes to monitor job, does NOT terminate job after timeout is reached",
+)
 
 
 def launch():
+    """The entry point into launching an experiment"""
+    args = parser.parse_args()
+    experiment_name: str = args.experiment_name
+    job_id: str = args.job_id
+    cpu_count: int = args.cpu
+    timeout_mins: int = args.timeout
+    docker_image_tag = "scenarios_image_%s" % job_id
     print(
-        "Launching experiment %s under jobid %s with %s cpu machines"
-        % (
-            experiment_name,
-            job_id,
-            cpu_count,
-        )
+        f"""{utils.bcolors.OKGREEN}Launching experiment {experiment_name}
+        under jobid {job_id} with {cpu_count} cpu machines{utils.bcolors.ENDC}
+        """
     )
-    print("example launching of the experiment from %s" % str(os.getcwd()))
+    azure_config_toml_path = os.path.join(
+        os.getcwd(), SECRETS_DIR_NAME, SECRETS_FILE_NAME
+    )
+
+    postprocess_execution_order = []
+    # upload dockerfile used
+    launcher = AzureExperimentLauncher(
+        experiment_name,
+        job_id,
+        azure_config_toml=azure_config_toml_path,
+        experiment_directory=EXP_DIR_NAME_OPTIONAL,
+        docker_image_name=docker_image_tag,
+    )
+    pp_scripts_path = os.path.join(
+        launcher.experiment_path_local, "postprocessing_scripts"
+    )
+    if os.path.exists(pp_scripts_path):
+        postprocess_script_filenames = [
+            f
+            for f in os.listdir(pp_scripts_path)
+            if os.path.isfile(os.path.join(pp_scripts_path, f))
+        ]
+        # Sort the list based on the numeric prefix of each filename
+        sorted_list = sorted(
+            postprocess_script_filenames, key=lambda x: int(x.split("_")[0])
+        )
+
+        # Group the filenames based on their shared numbers
+        postprocess_execution_order = [
+            list(group)
+            for _, group in groupby(sorted_list, lambda x: x.split("_")[0])
+        ]
+
+    launcher.set_resource_pool(pool_name="scenarios_%scpu_pool" % cpu_count)
+    all_tasks_run = []
+    state_task_ids = launcher.launch_states()
+    postprocessing_tasks = launcher.launch_postprocess(
+        execution_order=postprocess_execution_order,
+        depend_on_task_ids=state_task_ids,
+    )
+    all_tasks_run += state_task_ids + postprocessing_tasks
+    launcher.azure_client.monitor_job(job_id, timeout=timeout_mins)
