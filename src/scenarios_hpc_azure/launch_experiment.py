8323ea7dc9117886455761a0011b2fb3e67030ad
Ariel Shurygin <arik.shurygin@gmail.com>
2024-11-07 23:53:13 +0000

    checkpoing fixing some spacing problems and adding parsing of explicit_csv_path

--- src/scenarios_hpc_azure/launch_experiment.py
+++ src/scenarios_hpc_azure/launch_experiment.py
@@ -2,42 +2,37 @@ import argparse
 import os
 from itertools import groupby
 
+import pandas as pd
+
 from . import EXP_DIR_NAME, SECRETS_DIR_NAME, SECRETS_FILE_NAME, utils
 from .azure_utils import AzureExperimentLauncher
 
-description_str = """a script built to launch experiments created by
-the create_experiment script onto CFA's Azure hpc environment.
-
-implicitly this script requires a `secrets/` directory in the current working
-directory of the user in order to authenticate with Azure. All Azure
-related actions are performed using the `cfa_azure` repository here:
+description_str = """a script built to launch experiments created by the
+create_experiment script onto CFA's Azure hpc environment. Implicitly this
+script requires a `secrets/` directory in the current working directory
+of the user in order to authenticate with Azure. All Azure related actions are
+performed using the `cfa_azure` repository here:
 https://github.com/CDCgov/cfa_azure
 """
 epilog_str = """Note: the optional --explicit flag is meant to point
 to a CSV containing 1 column per flag passed to the `run_task.py`
-script running each state. For example if you wish to run 2 states (CA & NC)
-each with 2 scenarios (scenario1/2) and your `run_task.py` expects a
-`state` and `scenario` flag as command line arguments
-your csv would resemble something like: \n
-state,scenario\n
-CA,scenario1\n
-CA,scenario2\n
-NC,scenario1\n
-NC,scenario2\n
-
-It is important to note that the column titles are passed as the flag names
-with a `--` prepended, so the column names are important."""
+script running each state. It is important to note that the column titles are
+passed as the flag names with a `--` prepended,
+so the column names are important. It is possible to skip over states
+in --explicit mode, so programatic generation of the csv is encouraged"""
 
 parser = argparse.ArgumentParser(
-    prog="launch_experiment", description=description_str, epilog=epilog_str
+    prog="launch_experiment",
+    description=description_str,
+    epilog=epilog_str,
 )
 
-parser = argparse.ArgumentParser(description="Experiment Azure Launcher")
 parser.add_argument(
     "-j",
     "--job_id",
     type=str,
     help="job ID of the azure job, must be unique",
+    metavar="",
     required=True,
 )
 parser.add_argument(
@@ -46,6 +41,7 @@ parser.add_argument(
     type=str,
     help="the experiment name, must match the experiment directory within %s"
     % EXP_DIR_NAME,
+    metavar="",
     required=True,
 )
 parser.add_argument(
@@ -54,6 +50,7 @@ parser.add_argument(
     type=int,
     required=False,
     default=8,
+    metavar="",
     help="CPU count of machines running each task, supports 2, 4, or 8 cores",
 )
 
@@ -63,6 +60,7 @@ parser.add_argument(
     type=int,
     required=False,
     default=600,
+    metavar="",
     help="timeout time in minutes to monitor job, does NOT terminate job after timeout is reached",
 )
 
@@ -70,6 +68,7 @@ parser.add_argument(
     "--explicit",
     type=str,
     required=False,
+    metavar="",
     help="path to optional explicit task arguments csv",
 )
 
@@ -81,12 +80,14 @@ def launch():
     job_id: str = args.job_id
     cpu_count: int = args.cpu
     timeout_mins: int = args.timeout
-    # explicit_csv_path: str = args.explicit
+    explicit_csv_path: str = args.explicit
     docker_image_tag = "scenarios_image_%s" % job_id
     print(
-        f"""{utils.bcolors.OKGREEN}Launching experiment {experiment_name}
-        under jobid {job_id} with {cpu_count} cpu machines{utils.bcolors.ENDC}
-        """
+        (
+            f"{utils.bcolors.OKGREEN}Launching experiment "
+            f"{experiment_name} under jobid {job_id} with {cpu_count}"
+            f" cpu machines{utils.bcolors.ENDC}"
+        )
     )
     azure_config_toml_path = os.path.join(
         os.getcwd(), SECRETS_DIR_NAME, SECRETS_FILE_NAME
@@ -123,7 +124,16 @@ def launch():
 
     launcher.set_resource_pool(pool_name="scenarios_%scpu_pool" % cpu_count)
     all_tasks_run = []
-    state_task_ids = launcher.launch_states()
+    if explicit_csv_path is not None:  # launch in explicit mode
+        if os.path.exists(explicit_csv_path):
+            task_arguments_df = pd.read_csv(explicit_csv_path)
+            launcher.launch_states_explicitly(task_arguments_df)
+        else:
+            raise FileNotFoundError(
+                "Unable to locate explicit csv at %s" % explicit_csv_path
+            )
+    else:  # launch default behavior
+        state_task_ids = launcher.launch_states()
     postprocessing_tasks = launcher.launch_postprocess(
         execution_order=postprocess_execution_order,
         depend_on_task_ids=state_task_ids,
